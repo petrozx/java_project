0b3fe36724164771e4942841a37dda1fd6f13624
Petr Shaklein <petroz@inbox.ru>
2022-01-17 20:52:50 +1000

    dev

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    public static final String userBD = "petroz";
+    public static final String passwordBD = "198719pv";
+    public static final String urlBD = "jdbc:mysql://mysql.petroz.myjino.ru/petroz";
+    public static Connection con;
+    public static Statement stmt;
+    public static ResultSet rs;
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            Connection con = DriverManager.getConnection(urlBD,userBD,passwordBD);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            while(rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR BD STEP 1 " + e);
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (Exception e) {
+                System.out.println("ERROR BD " + e);
+            }
+        }
     }
 }
+
